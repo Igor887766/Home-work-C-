8e31283e1fb58898cd970e76d4f927e0a2816df6
Igor <887766@tut.by>
2022-10-20 20:49:34 +0300

    решил задачи к 1 семинару

--- Program.cs
+++ Program.cs
@@ -56,11 +56,12 @@ if (n % 2 == 0)
     }
 */
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-Console.Write ("Введите число: ");
+/*Console.Write ("Введите число: ");
 int n = Convert.ToInt32 (Console.ReadLine());
 int current = 1;
-       while (current < n)
-       {
-        Console.Write(current+1 + " ");
-        current+= 2;
-       }
+     while (current < n)
+    {
+    Console.Write(current+1 + " ");
+    current+= 2;
+    }
+*/
