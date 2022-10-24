953e635321acdd0cefbaa84404b2c8b63f0bcdf3
Igor887766 <115185728+Igor887766@users.noreply.github.com>
2022-10-24 22:17:22 +0300

    Add files via upload

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -5,9 +5,7 @@
 918 -> 1
 */
 
-
-// Функция вывода цифры стоящей в середине трехзначногно числа.
-int InCenter(int a)
+/*int InCenter(int a)
 {
     
     int result = ((a / 10) % 10);
@@ -26,15 +24,44 @@ else
 {
     Console.WriteLine("второе число = " + InCenter(number));
 }
+*/
 
 /*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-645 -> 5
+645 -> 53
+
 
 78 -> третьей цифры нет
 
 32679 -> 6
 */
+
+int ThirdNumber (int num)
+{
+ int result;
+    int num1;
+     while (num > 1000)
+    {
+    num1 = num / 10;
+    result = num % 10;
+    }
+  return result;
+}
+
+Console.WriteLine("введите число: ");
+
+int num11 = Convert.ToInt32(Console.ReadLine());
+
+if (num11 < 100)
+{
+         Console.WriteLine ("третьей цифры нет");
+}
+else
+{
+       int res = ThirdNumber (num11);
+
+        Console.Write("третья цифра = " (res));
+}
 
 
 /*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
@@ -42,4 +69,15 @@ else
 6 -> да
 7 -> да
 1 -> нет
+*/
+/*
+Console.Write("Введите цифру дня недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (0 < n && n < 8)
+    if (n==6 | n==7)
+    Console.Write("да");
+    else 
+    Console.Write("нет");
+ else 
+    Console.Write("нет такого дня недели");
 */
